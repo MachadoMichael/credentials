8b82908eea1bf8b27202a476c176f0415941b120
Michael <machado.m3@icloud.com>
2024-06-04 07:46:28 -0300

    feat: add logs package, with a logger. Inside this package we add startLogFile to load or create file for logs and get funcs to login and error loggers.

--- pkg/logs/logger.go
+++ pkg/logs/logger.go
@@ -1,12 +1,37 @@
 package logs
 
 import (
-	"log/slog"
+	"log"
+	"os"
 	"sync"
 )
 
 var (
-	loginLogger *slog.Logger
-	errorLogger *slog.Logger
+	loginLogger *log.Logger
+	errorLogger *log.Logger
 	once        sync.Once
 )
+
+func startLogFile(filePath string) *log.Logger {
+	file, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	return log.New(file, "", log.LstdFlags|log.Lshortfile)
+}
+
+func InitLoggers() {
+	once.Do(func() {
+		loginLogger = startLogFile("logs/login.log")
+		errorLogger = startLogFile("logs/error.log")
+	})
+}
+
+func LoginLogger() *log.Logger {
+	return loginLogger
+}
+
+func ErrorLogger() *log.Logger {
+	return errorLogger
+}
