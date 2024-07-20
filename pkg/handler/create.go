b18333f342efb3bbd9ed769be9f6fba38c14c144
Michael <machado.m3@icloud.com>
2024-07-20 18:14:50 -0300

    refactor: updating handler.Create

--- pkg/handler/create.go
+++ pkg/handler/create.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"encoding/json"
 	"net/http"
 	"unicode/utf8"
 
@@ -8,52 +9,59 @@ import (
 	"github.com/MachadoMichael/credentials/pkg/encrypt"
 	"github.com/MachadoMichael/credentials/pkg/logger"
 	"github.com/MachadoMichael/credentials/schema"
-	"github.com/gin-gonic/gin"
 	"golang.org/x/exp/slog"
 )
 
-func Create(ctx *gin.Context) {
-	request := schema.Credentials{}
-	if err := ctx.BindJSON(&request); err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		logger.ErrorLogger.Write(slog.LevelError, err.Error())
+func Create(w http.ResponseWriter, r *http.Request) {
+	credential := schema.Credentials{}
+	err := json.NewDecoder(r.Body).Decode(&credential)
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(w).Encode(err.Error())
 		return
 	}
 
-	cred, err := database.CredentialRepo.ReadOne(request.Email)
+	cred, err := database.CredentialRepo.ReadOne(credential.Email)
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error})
+		w.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(w).Encode(err.Error())
 		logger.ErrorLogger.Write(slog.LevelError, err.Error())
 		return
 	}
 
 	if cred != "" {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": "This email is already in use"})
+		w.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(w).Encode("This email is already in use")
 		logger.ErrorLogger.Write(slog.LevelError, "This email is already iin use")
 		return
 	}
 
-	if utf8.RuneCountInString(request.Password) < 6 {
-		ctx.JSON(http.StatusBadGateway, gin.H{"error": "The password must be longer than 6 characters."})
+	if utf8.RuneCountInString(credential.Password) < 6 {
+		w.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(w).Encode("The password must be longer than 6 characters.")
 		logger.ErrorLogger.Write(slog.LevelError, "The password must be longer than 6 characters.")
 		return
 	}
 
-	hash, hashErr := encrypt.HashPassword(request.Password)
+	hash, hashErr := encrypt.HashPassword(credential.Password)
 	if hashErr != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(w).Encode(err.Error())
 		logger.ErrorLogger.Write(slog.LevelError, hashErr.Error())
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": hashErr.Error})
 		return
 	}
 
-	request.Password = hash
-	errDb := database.CredentialRepo.Create(request)
+	credential.Password = hash
+	errDb := database.CredentialRepo.Create(credential)
 	if errDb != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": errDb.Error})
+		w.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(w).Encode(err.Error())
 		logger.ErrorLogger.Write(slog.LevelError, errDb.Error())
 		return
 	}
 
-	logger.AccessLogger.Write(slog.LevelInfo, "New Credential created successfully, email: "+request.Email)
-	ctx.JSON(http.StatusOK, gin.H{"message": "Credential created successfully"})
+	logger.AccessLogger.Write(slog.LevelInfo, "New Credential created successfully, email: "+credential.Email)
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode("Credential created successfully")
+
 }
