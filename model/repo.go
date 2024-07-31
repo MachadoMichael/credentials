d916a68d2dda4897a23e06a46b23fa70d83e9a2e
Michael <machado.m3@icloud.com>
2024-07-31 13:54:11 -0300

    fix: mention Credentials instead Credential

--- model/repo.go
+++ model/repo.go
@@ -4,5 +4,5 @@ type RepoService interface {
 	Read() (map[string]string, error)
 	ReadOne(email string) (string, error)
 	Delete(email string) (int64, error)
-	Create(cred Credentials) error
+	Create(cred Credential) error
 }
